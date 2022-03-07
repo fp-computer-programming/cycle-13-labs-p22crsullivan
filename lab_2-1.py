76e1bc43ebed6ed1db2bda23b092d739e1734b91
p22crsullivan <p22crsullivan@fairfieldprep.org>
2022-03-07 09:36:35 -0500

    Save Cycle 13 Labs

--- lab_2-1.py
+++ lab_2-1.py
@@ -1,11 +1,12 @@
 # Author: CRS 03/04/22
+# Define build_car function
 def build_car(wheels, axels, doors, color):
     return("This care has " + wheels + " wheels, " + axels + " axels, " + doors + " doors, and is " + color + ".")
 
-
+# Create variables for each part
 wheels = input("What wheels would you like?")
 axels = input("How many axels would you like?")
 doors = input("How many doors would you like?")
 color = input("What color would you like?")
-
+# Test the code
 print(build_car(wheels, axels, doors, color))
--- lab_2-2.py
+++ lab_2-2.py
@@ -1,13 +1,14 @@
 # Author: CRS 03/04/22
+# Define build_vehicle function
 def build_vehicle(vehicle_type, axels, doors, color, wheels, price_range):
     return("This care is a " + vehicle_type + " with " + wheels + " wheels, " + axels + " axels, " + doors + " doors, is " + color + ", and is roughly $" + price_range + ".")
 
-
+# Create variables for each part
 vehicle_type = input("What type of vehicle would you like?")
 axels = input("How many axels would you like?")
 doors = input("How many doors would you like?")
 color = input("What color would you like?")
 wheels = input("How many wheels would you like?")
 price_range = input("What is you price range for your vehicle?")
-
+# Test the code
 print(build_vehicle(vehicle_type, axels, doors, color, wheels, price_range))
